5272fbce0785e7e715c5a11ce7dc8c2eee7bfda8
Julian <fernandezjulian285@gmail.com>
2026-01-12 12:54:49 -0300

    cambio en el archivo index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const generatedId = ()=>{
     return idGenerado
 }
 
-const PORT =3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log("el servidor está corriendo en el puerto ",PORT)
 })
